3809aee18d2a1196d9f9e39b4f93be7e8fbf304d
saimukzi <saimukzi@hiauntie.com>
2024-09-01 12:28:52 +0800

    better debug coor

--- debug_coor.gd
+++ debug_coor.gd
@@ -1,49 +1,96 @@
 extends Node3D
 
-enum Direction {XP,XN,YP,YN,ZP,ZN}
+enum Direction {XP,XN,ZP,ZN,YXP,YXN,YZP,YZN}
 @export var direction:Direction
 @export var offset: Vector3i
+
+enum Axis {X,Y,Z}
+var _axis:Axis
+
+const DIRECTION_TO_AXIS_DICT = {
+	Direction.XP:Axis.X,
+	Direction.XN:Axis.X,
+	Direction.ZP:Axis.Z,
+	Direction.ZN:Axis.Z,
+	Direction.YXP:Axis.Y,
+	Direction.YXN:Axis.Y,
+	Direction.YZP:Axis.Y,
+	Direction.YZN:Axis.Y,
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var c:Color
 	if direction == Direction.XP:
-		self.rotate_y(PI*1.5)
+		$Offset.position = Vector3(0.5,0,2)
+		$Offset.rotate_y(PI*1.5)
+		$Offset.rotate_x(PI)
 		c = Color.RED
 	if direction == Direction.XN:
-		self.rotate_y(PI*0.5)
+		$Offset.position = Vector3(0.5,0,2)
+		$Offset.rotate_y(PI*1.5)
 		c = Color.RED
-	if direction == Direction.YP:
-		self.rotate_x(PI*0.5)
-		c = Color.GREEN
-	if direction == Direction.YN:
-		self.rotate_x(PI*1.5)
-		c = Color.GREEN
 	if direction == Direction.ZP:
-		self.rotate_y(PI)
+		$Offset.position = Vector3(2,0,0.5)
+		$Offset.rotate_y(PI)
+		$Offset.rotate_x(PI)
 		c = Color.BLUE
 	if direction == Direction.ZN:
+		$Offset.position = Vector3(2,0,0.5)
+		$Offset.rotate_y(PI)
 		c = Color.BLUE
-	$Label3D.modulate = Color.WHITE
-	$Label3D.modulate.a = 0.25
-	$Label3D.outline_modulate = c
-	$Label3D.outline_modulate.a = 0.25
+	if direction == Direction.YXP:
+		$Offset.position = Vector3(0,0.5,2)
+		$Offset.rotate_y(PI*1.5)
+		$Offset.rotate_z(PI*0.5)
+		c = Color.GREEN
+	if direction == Direction.YXN:
+		$Offset.position = Vector3(0,0.5,2)
+		$Offset.rotate_y(PI*0.5)
+		$Offset.rotate_z(PI*1.5)
+		c = Color.GREEN
+	if direction == Direction.YZP:
+		$Offset.position = Vector3(2,0.5,0)
+		$Offset.rotate_y(PI*1)
+		$Offset.rotate_x(PI*1.5)
+		c = Color.GREEN
+	if direction == Direction.YZN:
+		$Offset.position = Vector3(2,0.5,0)
+		$Offset.rotate_y(PI*0)
+		$Offset.rotate_x(PI*0.5)
+		c = Color.GREEN
+	$Offset/Label3D.modulate = Color.WHITE
+	$Offset/Label3D.modulate.a = 0.25
+	$Offset/Label3D.outline_modulate = c
+	$Offset/Label3D.outline_modulate.a = 0.25
+	self._axis = DIRECTION_TO_AXIS_DICT[direction]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-var position_i : Vector3i
+#var position_i : Vector3i
+var camera_global_position_4i : Vector3i = Vector3i.ONE
+var v : int = 1
 func _process(delta: float) -> void:
 	var parent = get_parent()
-	var new_position = offset + parent.camera_global_position_4i
-	if new_position != self.position_i:
-		self.position = Vector3(new_position)
-		self.position_i = new_position
-		var v = new_position.x if direction == Direction.XP else \
-				new_position.x if direction == Direction.XN else \
-				new_position.y if direction == Direction.YP else \
-				new_position.y if direction == Direction.YN else \
-				new_position.z if direction == Direction.ZP else \
-				new_position.z if direction == Direction.ZN else \
+	if  self.camera_global_position_4i != parent.camera_global_position_4i:
+		self.camera_global_position_4i = parent.camera_global_position_4i
+		var new_position_i:Vector3i = offset + parent.camera_global_position_4i
+		var new_position = Vector3(new_position_i)
+		#var axis = DIRECTION_TO_AXIS_DICT[direction]
+		if self.position != new_position:
+			self.position = new_position
+			if (self._axis==Axis.X) or (direction == Direction.YXP) or (direction == Direction.YXN):
+				self.visible = new_position_i.z % 8 == 0
+			if (self._axis==Axis.Z) or (direction == Direction.YZP) or (direction == Direction.YZN):
+				self.visible = new_position_i.x % 8 == 0
+		#var d = (axis == Axis.Z) or (direction == Direction.YXP) or (direction == Direction.YXN)
+		#var m8 = ((new_position.x+new_position.y+new_position.z) % 8 == 0)
+		#self.visible = d == m8
+		if not self.visible: return
+		var new_v = new_position_i.x if self._axis == Axis.X else \
+				new_position_i.y if self._axis == Axis.Y else \
+				new_position_i.z if self._axis == Axis.Z else \
 				0
-		var t = str(v)
-		$Label3D.text = t
+		if self.v != new_v:
+			self.v = new_v
+			$Offset/Label3D.text = str(new_v)
--- debug_coor_group.gd
+++ debug_coor_group.gd
@@ -6,37 +6,47 @@ var camera_global_position_4i:Vector3i
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(-2,10,4): for j in range(-2,10,4): for k in range(0,12,4):
-		var debug_coor_node
-
+	var debug_coor_node
+	for i in range(-8,16,4): for j in range(0,8,4): for k in range(-8,12,4):
 		debug_coor_node = debug_coor_tscn.instantiate()
 		debug_coor_node.direction = debug_coor_node.Direction.XN
-		debug_coor_node.offset = Vector3i(-k,i,j)
+		debug_coor_node.offset = Vector3i(i,-j,k)
 		self.add_child(debug_coor_node)
 
 		debug_coor_node = debug_coor_tscn.instantiate()
 		debug_coor_node.direction = debug_coor_node.Direction.XP
-		debug_coor_node.offset = Vector3i(k+4,i,j)
-		self.add_child(debug_coor_node)
-
-		debug_coor_node = debug_coor_tscn.instantiate()
-		debug_coor_node.direction = debug_coor_node.Direction.YN
-		debug_coor_node.offset = Vector3i(i,-k,j)
-		self.add_child(debug_coor_node)
-
-		debug_coor_node = debug_coor_tscn.instantiate()
-		debug_coor_node.direction = debug_coor_node.Direction.YP
-		debug_coor_node.offset = Vector3i(i,k+4,j)
+		debug_coor_node.offset = Vector3i(i,j+4,k)
 		self.add_child(debug_coor_node)
 
 		debug_coor_node = debug_coor_tscn.instantiate()
 		debug_coor_node.direction = debug_coor_node.Direction.ZN
-		debug_coor_node.offset = Vector3i(i,j,-k)
+		debug_coor_node.offset = Vector3i(k,-j,i)
 		self.add_child(debug_coor_node)
 
 		debug_coor_node = debug_coor_tscn.instantiate()
 		debug_coor_node.direction = debug_coor_node.Direction.ZP
-		debug_coor_node.offset = Vector3i(i,j,k+4)
+		debug_coor_node.offset = Vector3i(k,j+4,i)
+		self.add_child(debug_coor_node)
+
+	for i in range(-8,12,4): for y in range(-8,16,4):
+		debug_coor_node = debug_coor_tscn.instantiate()
+		debug_coor_node.direction = debug_coor_node.Direction.YXP
+		debug_coor_node.offset = Vector3i(12, y, i)
+		self.add_child(debug_coor_node)
+
+		debug_coor_node = debug_coor_tscn.instantiate()
+		debug_coor_node.direction = debug_coor_node.Direction.YXN
+		debug_coor_node.offset = Vector3i(-8, y, i)
+		self.add_child(debug_coor_node)
+
+		debug_coor_node = debug_coor_tscn.instantiate()
+		debug_coor_node.direction = debug_coor_node.Direction.YZP
+		debug_coor_node.offset = Vector3i(i, y, 12)
+		self.add_child(debug_coor_node)
+
+		debug_coor_node = debug_coor_tscn.instantiate()
+		debug_coor_node.direction = debug_coor_node.Direction.YZN
+		debug_coor_node.offset = Vector3i(i, y, -8)
 		self.add_child(debug_coor_node)
 
 
